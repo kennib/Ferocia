8747df19c45886ffb3f217ca7cc9c361a50004b6
Kenni <kenni.bawden@gmail.com>
2025-07-09 09:00:19 +0000

    Added basic server logic

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-from flask import Flask
+from flask import Flask, request, render_template
+
+import locale
+
+from calculations import term_deposit_interest
 
 app = Flask(__name__)
 
-
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+@app.route("/",  methods=['GET', 'POST'])
+def term_deposit_interest_form():
+    if request.method == 'POST':
+        final_balance = term_deposit_interest(
+            int(request.form['deposit']),
+            float(request.form['interest']),
+            int(request.form['term']),
+            "at maturity",
+        )
+        return render_template('form.html', final_balance=f'${final_balance:,.2f}')
+    else:
+        return render_template('form.html')
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
