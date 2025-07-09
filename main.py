f3d9235db312729d90136dfa1b2ea1a96bd3af44
Kenni <kenni.bawden@gmail.com>
2025-07-09 09:41:57 +0000

    Added form controls for payment schedule

--- main.py
+++ main.py
@@ -13,12 +13,13 @@ def term_deposit_interest_form():
             int(request.form['deposit']),
             float(request.form['interest']),
             int(request.form['term']),
-            "at maturity",
+            request.form['schedule'],
         )
         return render_template('form.html',
             deposit=int(request.form['deposit']),
             interest=float(request.form['interest']),
             term=int(request.form['term']),
+            schedule=request.form['schedule'],
             final_balance=f'${final_balance:,.2f}',
         )
     else:
@@ -26,6 +27,7 @@ def term_deposit_interest_form():
             deposit="10000",
             interest="1.10",
             term="3",
+            schedule="at maturity",
         )
 
 
